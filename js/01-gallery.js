c6dccb7eadd851b70841a774be3a5c420f787feb
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-07-14 14:36:27 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+// console.log(galleryItems);
+const ulEl = document.querySelector('.gallery');
+// Створення масиву елементів
+const markupLiEl = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+//Додавання всіх елементів за один раз
+ulEl.insertAdjacentHTML('beforeend', markupLiEl);
+// Прослуховування подій на всьому списку
+ulEl.addEventListener('click', handleClick);
+// Функція обробки події клік по картинці
+function handleClick(event) {
+  event.preventDefault();
+  //   console.log(event.target.dataset.source);
+  // Виклик модального вікна бібліотекою basicLightbox
+  basicLightbox
+    .create(
+      `
+  		<img width="1280px" src="${event.target.dataset.source}">
+  	`
+    )
+    .show();
+}
